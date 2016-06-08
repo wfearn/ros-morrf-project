9f077824bab269bc569d7d3fc2d2a11d9401307c
Timnah <man1f3s7@gmail.com>
2016-06-08 13:48:38 -0600

    Added costmap generator

---
+++ costmap_generator/generator.h
@@ -0,0 +1,60 @@
+﻿#ifndef GENERATOR_H
+#define GENERATOR_H
+#include <iostream>
+#include <list>
+#include <vector>
+#include <math.h>
+#include <set>
+#include <string>
+#include <sstream>
+#include "point.h"
+#include <QString>
+#include <QPixmap>
+#include <QImage>
+#include <QColor>
+#include <QPoint>
+#include <QPainter>
+#include <fstream>
+#include <QFileDialog>
+#include <QDebug>
+
+using namespace std;
+
+const double MIN_GRAYSCALE_VALUE = 10;
+const double MAX_GRAYSCALE_VALUE = 255;
+const QColor WHITE = QColor(Qt::white);
+const QColor BLACK = QColor(Qt::black);
+
+class Generator
+{
+
+private:
+    double diag_distance;
+    double slope;
+    double y_intercept;
+    double delta;
+    double maxProbOfSeenVal;
+    double minProbOfSeenVal;
+    int width;
+    int height;
+
+    list<Point> obsBoundaryPts;
+    set<string> allObsPts, enemyPtsToIgnore;
+
+    void getAllObsPts(QImage image);
+    void getEnemyPtsToIgnore(list<Point> enemyPts);
+    bool isOnLineSegment(Point a, Point b, Point c);
+    void writeImage(string file, string output, vector<vector<double>> imgProbVals);
+    void resize(vector<vector<double>> & array);
+    vector<double> setEnemyProbArray(int i, bool blocked, double distance);
+    bool isBlocked(Point imgPt, Point enemyPt);
+
+public:
+    Generator();
+    void probOfSeenByEnemy(double delta, list<Point> enemyPts, string world_boundaries, string world_solids, string imageFile, string outputFile);
+    void getImgBoundaryPts(QImage image);
+    void probOfBeingNearToObstacle(double delta, string world_boundaries, string world_solids, list<Point> enemyPts, string safeObjImgFilename, string result);
+
+};
+
+#endif // GENERATOR_H
