26bd7c9432e8fb10a85d08618494cd654ffdf5d8
wfearn <wilson.fearn@gmail.com>
2016-07-20 16:18:13 -0600

    Added the april tag follower node

--- april_tag_follower/scripts/listener.py
+++ april_tag_follower/scripts/listener.py
@@ -6,50 +6,50 @@ from geometry_msgs.msg import Pose2D
 
 def follow(data, point):
 
-	for i in range(0, len(data.id)):
-		rob_id = data.id[i]
-		rob_position = data.pos[i]
-		
-		v = .1
-		ang_vel = .001
+    for i in range(0, len(data.id)):
+        rob_id = data.id[i]
+        rob_position = data.pos[i]
 
-		p1 = (point.x, point.y)
-		p2 = rob_position
+        v = .1
+        ang_vel = .001
 
-		while dist(p1, p2) > 1:
-			theta = math.atan2( (p1[1] - p2[1]), (p1[0] - p2[0]) )
+        p1 = (point.x, point.y)
+        p2 = rob_position
 
-			dx = v * math.cos(theta)
-			dy = v * math.sin(theta)	
-			
-			p2 = (p2[0] + dx, p2[1] + dy)
-			data.pos[i].x = p2[0]
-			data.pos[i].y = p2[1]
+        while dist(p1, p2) > 1:
+            theta = math.atan2( (p1[1] - p2[1]), (p1[0] - p2[0]) )
+
+            dx = v * math.cos(theta)
+            dy = v * math.sin(theta)
+
+            p2 = (p2[0] + dx, p2[1] + dy)
+            data.pos[i].x = p2[0]
+            data.pos[i].y = p2[1]
 
 
 def update_orientation(theta, data):
-	rob_orient = data.orientation
-			
-	while abs(theta - rob_orient) > 0.01:
+    rob_orient = data.orientation
 
-		if rob_orient - theta > 0:
-			rob_orient -= ang_vel 
-		else:
-			rob_orient += ang_vel
+        while abs(theta - rob_orient) > 0.01:
 
-	data.orientation = rob_orient
-				
+            if rob_orient - theta > 0:
+                rob_orient -= ang_vel
+            else:
+                rob_orient += ang_vel
+
+            data.orientation = rob_orient
+
 
 def listener(data, next_point):
-	
-	rospy.init_node("listener")
 
-	rospy.Subscriber("april_tag_pos", data, next_point, follow)
+    rospy.init_node("listener")
 
-	rospy.spin()
+    rospy.Subscriber("april_tag_pos", data, next_point, follow)
+
+    rospy.spin()
 
 
 def dist (p1, p2):
-	return math.sqrt( (p2[0] - p1[0]) **2 + (p2[1] - p2[1]) **2 )
+    return math.sqrt( (p2[0] - p1[0]) **2 + (p2[1] - p2[1]) **2 )
 
 
---
+++ april_tag_follower/scripts/run_follower.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from follower import Follower
+
+def main():
+    f = Follower()
+
+if __name__ == "__main__":
+    main()
+
