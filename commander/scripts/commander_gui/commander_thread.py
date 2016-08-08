03b8a67082b359a009953e5feb7bc7adb888740a
wfearn <wilson.fearn@gmail.com>
2016-08-08 15:57:39 -0600

    Forgot to add custom Qthread classes

---
+++ commander/scripts/commander_gui/commander_thread.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import sys
+
+from publishers.commander_publisher import StartCommanderPublisher
+
+from morrf_ros.msg import *
+from morrf_ros.srv import *
+
+from PyQt4 import QtGui, QtCore
+
+
+class CommanderThread(QtCore.QThread):
+    def __init__(self, morrf_init, parent = None):
+        super(CommanderThread, self).__init__(parent)
+
+        self.init = morrf_init
+
+
+    def run(self):
+        print "Running commander thread"
+        response = StartCommanderPublisher(self.init)
+
+        print "Emitting morrf response"
+        self.emit(QtCore.SIGNAL("MORRF_RESPONSE"), response)
---
+++ commander/scripts/commander_gui/costmap_thread.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+
+from publishers.costmap_publisher import StartCostmapPublisher
+
+from morrf_ros.msg import *
+from morrf_ros.srv import *
+
+from commander.msg import *
+from commander.srv import *
+
+from PyQt4 import QtGui, QtCore
+
+
+class CostmapThread(QtCore.QThread):
+    def __init__(self, map_img, stealth, safe, locs, parent = None):
+        super(CostmapThread, self).__init__(parent)
+
+        self.img = map_img
+        self.stealth = stealth
+        self.safe = safe
+        self.locs = locs
+
+
+    def run(self):
+        print "Running costmap thread"
+        response = StartCostmapPublisher(self.img, self.stealth, self.safe, self.locs)
+
+        print "Emitting costmap response"
+        self.emit(QtCore.SIGNAL("COSTMAP_RESPONSE"), response)
